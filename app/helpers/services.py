c941c5367946d826763c44a482f6d04292c978b5
Jose Anicama <jose.anicama@itware.com.pe>
2024-05-26 18:00:09 -0500

    Add helper with Selenium

---
+++ app/helpers/services.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import re
+import unidecode
+
+class WebdriverService:
+    def __init__(self, url):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')  # Ejecutar Chrome en modo headless (sin interfaz gráfica)
+        options.add_argument('--no-sandbox')  # Solucionar problemas de permisos en algunos sistemas
+        options.add_argument('--disable-dev-shm-usage')  # Usar /tmp en lugar de /dev/shm
+        
+        service = Service(executable_path='/usr/bin/chromedriver')
+        
+        self.driver = webdriver.Chrome(service=service, options=options)
+        
+        self.driver.get(url)
+   
+    def getElementByPath(self, string_path):
+        return WebDriverWait(self.driver, 30).until(
+            EC.visibility_of_element_located((By.XPATH, f""+string_path))
+        )
+        
+
+def convert_to_slug(text):
+    
+    text = re.sub(r'\W+', '_', 
+            unidecode.unidecode(text).lower()
+        ).strip('_')
+
+    return text
+ 
